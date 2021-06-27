3c047ba3fdbf9e2d6e078ef370a4681f36713a6b
Lukas Heinrich <lukas.heinrich@gmail.com>
2021-06-27 19:16:49 +0200

    data.py

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 python table.py create-table test_replace_dot_remove_long.parquet physlitetestdata
 python table.py delete-table physlitetestdata
 ```
+
+```
+python table.py create-table test_replace_dot_remove_long.parquet physlitetestdata
+python table.py delete-table physlitetestdata
+```
---
+++ data.py
@@ -0,0 +1,57 @@
+import pyarrow.parquet as pq
+import awkward as ak
+import json
+import click
+from google.cloud import bigquery
+from google.api_core.exceptions import BadRequest
+
+@click.group()
+def main():
+    pass
+
+@main.command()
+@click.argument('parquet-file')
+@click.argument('jsonl-file')
+def prep_jsonl(parquet_file,jsonl_file):
+    data = ak.from_parquet(parquet_file)
+
+    what = json.loads(ak.to_json(data[:10]))
+
+    with open(jsonl_file,'w') as f:
+        for d in what:
+            for k in list(d.keys()):
+                if k in ['TrigMatchedObjects']:
+                    d.pop(k)
+            d['METAssoc_MET'] = []
+            f.write(f'{json.dumps(d)}'+'\n')
+
+@main.command()
+@click.argument('jsonl-file')
+@click.argument('table_name')
+@click.option('--gcp-project', default = 'gke-dev-311213')
+@click.option('--gcp-dataset', default = 'PHYSLITE100TB')
+def load_data(jsonl_file,table_name,gcp_project,gcp_dataset):
+    client = bigquery.Client()
+
+    table_id = f"{gcp_project}.{gcp_dataset}.{table_name}"
+
+    job_config = bigquery.LoadJobConfig(
+        source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
+    )
+
+    with open(jsonl_file, "rb") as source_file:
+        load_job = client.load_table_from_file(
+            source_file, table_id, job_config=job_config
+        )
+    try:
+        load_job.result()
+    except BadRequest as ex:
+        for err in ex.errors:
+            print(err)
+
+    destination_table = client.get_table(table_id)
+    click.secho("Table now has {} rows.".format(destination_table.num_rows))
+
+
+if __name__ == '__main__':
+    main()
